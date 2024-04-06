64a55b2f34f74f156a0f71c6e99465fbdc877996
Sidharth Kumar <85510647+stargalad@users.noreply.github.com>
2024-04-06 11:42:48 +0530

    Update solution4.js

--- solution4.js
+++ solution4.js
@@ -1,13 +1,15 @@
-import express from "express";
-import bodyParser from "body-parser";
-import { dirname } from "path";
+// Importing required modules
+import express from "express"; // Express.js for creating the server.
+import bodyParser from "body-parser"; // body-parser for parsing incoming request bodies
+import { dirname } from "path"; //Node.js built-in modules path and url for working with file and URL paths
 import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
+// Setting up the directory path
+const __dirname = dirname(fileURLToPath(import.meta.url)); // gets directory name of current module file using dirname() function from the path module. 
+// Creating an Express application instance and setting the port
 const app = express();
 const port = 3000;
-var bandName = "";
+var bandName = ""; // Initializing a variable to store band name
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
